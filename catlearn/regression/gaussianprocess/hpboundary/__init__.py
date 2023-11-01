475ea7bb06c3912ed7ca049873de4c22b96d82e9
Andreas Lynge Vishart <andreasvishart@hotmail.com>
2023-11-01 13:39:13 +0100

    Enable updating bounds from previous solutions

--- catlearn/regression/gaussianprocess/hpboundary/__init__.py
+++ catlearn/regression/gaussianprocess/hpboundary/__init__.py
@@ -4,6 +4,7 @@ from .restricted import RestrictedBoundaries
 from .educated import EducatedBoundaries
 from .strict import StrictBoundaries
 from .hptrans import VariableTransformation
+from .updatebounds import UpdatingBoundaries
 
 __all__ = ["HPBoundaries","LengthBoundaries","RestrictedBoundaries","EducatedBoundaries","StrictBoundaries",\
-           "VariableTransformation"]
+           "VariableTransformation","UpdatingBoundaries"]
