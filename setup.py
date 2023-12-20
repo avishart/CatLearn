443c427666120820b607a5a5d9bbade155cba2b2
Andreas Lynge Vishart <andreasvishart@hotmail.com>
2023-12-20 13:04:21 +0100

    Update version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '5.1.7' 
+VERSION = '5.1.8' 
 DESCRIPTION = 'Machine Learning using atomic-scale calculations'
 LONG_DESCRIPTION = 'Machine Learning using atomic-scale calculations'
 
