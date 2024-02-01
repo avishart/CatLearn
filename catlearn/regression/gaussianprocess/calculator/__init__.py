09ddf60ec774bb44f460eb66947f2d5862c6eb2b
Andreas Lynge Vishart <andreasvishart@hotmail.com>
2024-02-01 15:22:51 +0100

    Insert DatabasePointsInterestEach into init

--- catlearn/regression/gaussianprocess/calculator/__init__.py
+++ catlearn/regression/gaussianprocess/calculator/__init__.py
@@ -1,6 +1,6 @@
 from .database import Database
 from .copy_atoms import copy_atoms
-from .database_reduction import Database_Reduction,DatabaseDistance,DatabaseHybrid,DatabaseMin,DatabaseRandom,DatabaseLast,DatabaseRestart,DatabasePointsInterest
+from .database_reduction import Database_Reduction,DatabaseDistance,DatabaseHybrid,DatabaseMin,DatabaseRandom,DatabaseLast,DatabaseRestart,DatabasePointsInterest,DatabasePointsInterestEach
 from .mlmodel import MLModel,get_default_model,get_default_database,get_default_mlmodel
 from .mlcalc import MLCalculator
 from .bocalc import BOCalculator
@@ -8,6 +8,7 @@ from .bocalc import BOCalculator
 __all__ = ["Database",
            "copy_atoms",
            "Database_Reduction","DatabaseDistance","DatabaseHybrid",
-           "DatabaseMin","DatabaseRandom","DatabaseLast","DatabaseRestart","DatabasePointsInterest",
+           "DatabaseMin","DatabaseRandom","DatabaseLast","DatabaseRestart",
+           "DatabasePointsInterest","DatabasePointsInterestEach",
            "MLModel","get_default_model","get_default_database","get_default_mlmodel",
            "MLCalculator","BOCalculator"]
