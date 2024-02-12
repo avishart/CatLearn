64b206b0510c18d360784c5030fb0f5c762b4eea
Andreas Lynge Vishart <andreasvishart@hotmail.com>
2024-02-12 11:34:03 +0100

    Insert the HierarchicalMLModel in init

--- catlearn/regression/gaussianprocess/calculator/__init__.py
+++ catlearn/regression/gaussianprocess/calculator/__init__.py
@@ -2,6 +2,7 @@ from .database import Database
 from .copy_atoms import copy_atoms
 from .database_reduction import Database_Reduction,DatabaseDistance,DatabaseHybrid,DatabaseMin,DatabaseRandom,DatabaseLast,DatabaseRestart,DatabasePointsInterest,DatabasePointsInterestEach
 from .mlmodel import MLModel,get_default_model,get_default_database,get_default_mlmodel
+from .hiermodel import HierarchicalMLModel
 from .mlcalc import MLCalculator
 from .bocalc import BOCalculator
 
@@ -11,4 +12,5 @@ __all__ = ["Database",
            "DatabaseMin","DatabaseRandom","DatabaseLast","DatabaseRestart",
            "DatabasePointsInterest","DatabasePointsInterestEach",
            "MLModel","get_default_model","get_default_database","get_default_mlmodel",
+           "HierarchicalMLModel",
            "MLCalculator","BOCalculator"]
