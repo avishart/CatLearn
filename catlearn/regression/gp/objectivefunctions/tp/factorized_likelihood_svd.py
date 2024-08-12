0672ab77369dca697d8f674ed4cfc8a4f519e08d
Andreas Lynge Vishart <andreasvishart@hotmail.com>
2024-08-12 16:13:53 +0200

    Solve flake8 conflicts

--- catlearn/regression/gp/objectivefunctions/tp/factorized_likelihood_svd.py
+++ catlearn/regression/gp/objectivefunctions/tp/factorized_likelihood_svd.py
@@ -2,36 +2,52 @@ import numpy as np
 from .factorized_likelihood import FactorizedLogLikelihood
 from numpy.linalg import svd
 
+
 class FactorizedLogLikelihoodSVD(FactorizedLogLikelihood):
-    def __init__(self,get_prior_mean=False,ngrid=80,bounds=None,noise_optimizer=None,**kwargs):
-        """ 
-        The factorized log-likelihood objective function that is used to optimize the hyperparameters. 
-        A SVD is performed to get the eigenvalues. 
-        The relative-noise hyperparameter can be searched from a single eigendecomposition for each length-scale hyperparameter. 
+    def __init__(
+        self,
+        get_prior_mean=False,
+        ngrid=80,
+        bounds=None,
+        noise_optimizer=None,
+        **kwargs
+    ):
+        """
+        The factorized log-likelihood objective function that is used
+        to optimize the hyperparameters.
+        A SVD is performed to get the eigenvalues.
+        The relative-noise hyperparameter can be searched from
+        a single eigendecomposition for each length-scale hyperparameter.
+
         Parameters:
             get_prior_mean: bool
-                Whether to save the parameters of the prior mean in the solution.
+                Whether to save the parameters of the prior mean
+                in the solution.
             ngrid: int
-                Number of grid points that are searched in the relative-noise hyperparameter. 
+                Number of grid points that are searched in
+                the relative-noise hyperparameter.
             bounds: Boundary_conditions class
-                A class of the boundary conditions of the relative-noise hyperparameter.
+                A class of the boundary conditions of
+                the relative-noise hyperparameter.
             noise_optimizer : Noise line search optimizer class
-                A line search optimization method for the relative-noise hyperparameter.
+                A line search optimization method for
+                the relative-noise hyperparameter.
         """
-        super().__init__(get_prior_mean=get_prior_mean,
-                         ngrid=ngrid,
-                         bounds=bounds,
-                         noise_optimizer=noise_optimizer,
-                         **kwargs)
-    
-    def get_eig(self,model,X,Y):
-        " Calculate the eigenvalues " 
+        super().__init__(
+            get_prior_mean=get_prior_mean,
+            ngrid=ngrid,
+            bounds=bounds,
+            noise_optimizer=noise_optimizer,
+            **kwargs
+        )
+
+    def get_eig(self, model, X, Y):
+        "Calculate the eigenvalues"
         # Calculate the kernel with and without noise
-        KXX,n_data=self.kxx_corr(model,X)
+        KXX, n_data = self.kxx_corr(model, X)
         # SVD
-        U,D,Vt=svd(KXX,hermitian=True)
+        U, D, Vt = svd(KXX, hermitian=True)
         # Subtract the prior mean to the training target
-        Y_p=self.y_prior(X,Y,model,D=D,U=U)
-        UTY=np.matmul(Vt,Y_p).reshape(-1)**2
-        return D,U,Y_p,UTY,KXX,n_data
-    
+        Y_p = self.y_prior(X, Y, model, D=D, U=U)
+        UTY = np.matmul(Vt, Y_p).reshape(-1) ** 2
+        return D, U, Y_p, UTY, KXX, n_data
