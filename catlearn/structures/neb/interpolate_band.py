f25cc1c59b24cbae2bf2be3a378056fd9db33744
Andreas Lynge Vishart <andreasvishart@hotmail.com>
2025-05-21 09:42:07 +0200

    Use the endpoints in the NEB interpolations and implement the born interpolation.

--- catlearn/structures/neb/interpolate_band.py
+++ catlearn/structures/neb/interpolate_band.py
@@ -1,5 +1,7 @@
-import numpy as np
+from numpy import ndarray
+from numpy.linalg import norm
 from ase.io import read
+from ase.geometry import find_mic
 from ase.optimize import FIRE
 from ase.build import minimize_rotation_and_translation
 from ...regression.gp.calculator.copy_atoms import copy_atoms
@@ -16,8 +18,40 @@ def interpolate(
     **interpolation_kwargs,
 ):
     """
-    Make an interpolation between the start and end structure.
-    A transition state structure can be given to guide the interpolation.
+    Make a NEB interpolation between the start and end structure.
+    A transition state structure can be given to guide the NEB interpolation.
+
+    Parameters:
+        start: ASE Atoms instance
+            The starting structure for the NEB interpolation.
+        end: ASE Atoms instance
+            The ending structure for the NEB interpolation.
+        ts: ASE Atoms instance (optional)
+            An intermediate state the NEB interpolation should go through.
+            Then, the method should be one of the following: 'linear', 'idpp',
+            'rep', 'born', or 'ends'.
+        n_images: int
+            The number of images in the NEB interpolation.
+        method: str or list of ASE Atoms instances
+            The method to use for the NEB interpolation. If a list of
+            ASE Atoms instances is given, then the interpolation will be
+            made between the start and end structure using the images in
+            the list. If a string is given, then it should be one of the
+            following: 'linear', 'idpp', 'rep', or 'ends'. The string can
+            also be the name of a trajectory file. In that case, the
+            interpolation will be made using the images in the trajectory
+            file. The trajectory file should contain the start and end
+            structure.
+        mic: bool
+            If True, then the minimum-image convention is used for the
+            interpolation. If False, then the images are not constrained
+            to the minimum-image convention.
+        remove_rotation_and_translation: bool
+            If True, then the rotation and translation of the end
+            structure is removed before the interpolation is made.
+        interpolation_kwargs: dict
+            Additional keyword arguments to pass to the interpolation
+            methods.
     """
     # Copy the start and end structures
     start = copy_atoms(start)
@@ -42,6 +76,15 @@ def interpolate(
         return images
     # Copy the transition state structure
     ts = copy_atoms(ts)
+    # Check if the method is compatible with the interpolation for the TS
+    if not (
+        isinstance(method, str)
+        and method in ["linear", "idpp", "rep", "born", "ends"]
+    ):
+        raise ValueError(
+            "The method should be one of the following: "
+            "'linear', 'idpp', 'rep', 'born', or 'ends."
+        )
     # Get the interpolated path from the start structure to the TS structure
     images = make_interpolation(
         start,
@@ -104,19 +147,22 @@ def make_interpolation(
 ):
     "Make the NEB interpolation path."
     # Use a premade interpolation path
-    if isinstance(method, (list, np.ndarray)):
-        images = [copy_atoms(image) for image in method]
+    if isinstance(method, (list, ndarray)):
+        images = [copy_atoms(image) for image in method[1:-1]]
+        images = [copy_atoms(start)] + images + [copy_atoms(end)]
     elif isinstance(method, str) and method.lower() not in [
         "linear",
         "idpp",
         "rep",
+        "born",
         "ends",
     ]:
         # Import interpolation from a trajectory file
         images = read(method, "-{}:".format(n_images))
+        images = [copy_atoms(start)] + images[1:-1] + [copy_atoms(end)]
     else:
         # Make path by the NEB methods interpolation
-        images = [start.copy() for i in range(1, n_images - 1)]
+        images = [start.copy() for _ in range(1, n_images - 1)]
         images = [copy_atoms(start)] + images + [copy_atoms(end)]
         if method.lower() == "ends":
             images = make_end_interpolations(
@@ -142,13 +188,17 @@ def make_interpolation(
                     mic=mic,
                     **interpolation_kwargs,
                 )
+            elif method.lower() == "born":
+                images = make_born_interpolation(
+                    images,
+                    mic=mic,
+                    **interpolation_kwargs,
+                )
     return images
 
 
 def make_linear_interpolation(images, mic=False, **kwargs):
     "Make the linear interpolation from initial to final state."
-    from ase.geometry import find_mic
-
     # Get the position of initial state
     pos0 = images[0].get_positions()
     # Get the distance to the final state
@@ -212,6 +262,9 @@ def make_rep_interpolation(
     steps=100,
     local_opt=FIRE,
     local_kwargs={},
+    calc_kwargs={},
+    trajectory="rep.traj",
+    logfile="rep.log",
     **kwargs,
 ):
     """
@@ -222,11 +275,48 @@ def make_rep_interpolation(
 
     # Use Repulsive potential as calculator
     for image in images[1:-1]:
-        image.calc = RepulsionCalculator(power=10, mic=mic)
+        image.calc = RepulsionCalculator(mic=mic, **calc_kwargs)
     # Make default NEB
     neb = ImprovedTangentNEB(images)
     # Set local optimizer arguments
-    local_kwargs_default = dict(trajectory="rep.traj", logfile="rep.log")
+    local_kwargs_default = dict(trajectory=trajectory, logfile=logfile)
+    if isinstance(local_opt, FIRE):
+        local_kwargs_default.update(
+            dict(dt=0.05, a=1.0, astart=1.0, fa=0.999, maxstep=0.2)
+        )
+    local_kwargs_default.update(local_kwargs)
+    # Optimize NEB path with repulsive potential
+    with local_opt(neb, **local_kwargs_default) as opt:
+        opt.run(fmax=fmax, steps=steps)
+    return images
+
+
+def make_born_interpolation(
+    images,
+    mic=False,
+    fmax=1.0,
+    steps=100,
+    local_opt=FIRE,
+    local_kwargs={},
+    calc_kwargs={},
+    trajectory="born.traj",
+    logfile="born.log",
+    **kwargs,
+):
+    """
+    Make a Born repulsive potential to get the interpolation from NEB
+    optimization.
+    """
+    from .improvedneb import ImprovedTangentNEB
+    from ...regression.gp.baseline import BornRepulsionCalculator
+
+    # Use Repulsive potential as calculator
+    for image in images[1:-1]:
+        image.calc = BornRepulsionCalculator(mic=mic, **calc_kwargs)
+    # Make default NEB
+    neb = ImprovedTangentNEB(images)
+    # Set local optimizer arguments
+    local_kwargs_default = dict(trajectory=trajectory, logfile=logfile)
     if isinstance(local_opt, FIRE):
         local_kwargs_default.update(
             dict(dt=0.05, a=1.0, astart=1.0, fa=0.999, maxstep=0.2)
@@ -256,7 +346,7 @@ def make_end_interpolations(images, mic=False, trust_dist=0.2, **kwargs):
     if mic:
         dist = find_mic(dist, images[0].get_cell(), images[0].pbc)[0]
     # Calculate the scaled distance
-    scale_dist = 2.0 * trust_dist / np.linalg.norm(dist)
+    scale_dist = 2.0 * trust_dist / norm(dist)
     # Check if the distance is within the trust distance
     if scale_dist >= 1.0:
         # Calculate the distance moved for each image
@@ -283,7 +373,5 @@ def get_images_distance(images):
     "Get the cumulative distacnce of the images."
     dis = 0.0
     for i in range(len(images) - 1):
-        dis += np.linalg.norm(
-            images[i + 1].get_positions() - images[i].get_positions()
-        )
+        dis += norm(images[i + 1].get_positions() - images[i].get_positions())
     return dis
